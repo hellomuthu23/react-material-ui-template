a938dccdaec39e234bb996d3c36cb37ed156922f
Muthu Pandian <muthu.pandian@gfk.com>
2021-05-31 01:40:37 +0100

    feat: Add Kanban style board

--- src/components/Boards/BoardArea/BoardArea.tsx
+++ src/components/Boards/BoardArea/BoardArea.tsx
@@ -1,20 +1,63 @@
-import { Button } from '@material-ui/core';
+import { Button, Card } from '@material-ui/core';
 import React, { useState } from 'react';
-import { Board } from '../../../types/board';
+import { Board, Task } from '../../../types/board';
 import { AddTask } from '../../Tasks/AddTask/AddTask';
-import { Tasks} from '../../Tasks/Tasks';
+import { TaskCard } from '../../Tasks/TaskCard/TaskCard';
 import './BoardArea.css';
+
+import ReactBoard from '@lourenci/react-kanban';
+import '@lourenci/react-kanban/dist/styles.css';
+import { Status } from '../../../types/status';
 
 interface BoardAreaProps {
   board: Board;
 }
 export const BoardArea: React.FC<BoardAreaProps> = ({ board }) => {
-  const [showAddTask, setShowAddTask] = useState(false)
+  const [showAddTask, setShowAddTask] = useState(false);
+  const todoCards = board.tasks.map((task) => {
+    if (task.status === Status.NotStarted) {
+      return {
+        id: task.id,
+        content: {
+          name: task.name,
+          description: task.description || '',
+        },
+      };
+    }
+  });
+  const data = {
+    columns: [
+      {
+        id: 1,
+        title: 'Todo',
+        cards: todoCards,
+      },
+      {
+        id: 2,
+        title: 'Doing',
+        cards: [
+          {
+            id: 3,
+            content: {
+              title: 'Drag-n-drop support',
+              description: 'Move a card between the columns',
+            },
+          },
+        ],
+      },
+    ],
+  };
+
+  // ts-ignore
+  const renderCard = ({ content }, { removeCard, dragging }) => (
+    <TaskCard boardId={board.id} task={content as Task}></TaskCard>
+  );
   return (
     <>
       <div className='ContentArea'>{board.name}</div>
-      <Tasks boardId={board.id}></Tasks>
-      <Button onClick={()=>setShowAddTask(true)}>Add Task</Button>
+      <ReactBoard allowAddCard initialBoard={data} renderCard={renderCard} />
+
+      <Button onClick={() => setShowAddTask(true)}>Add Task</Button>
       {showAddTask && <AddTask boardId={board.id}></AddTask>}
       <div className='Footer'></div>
     </>
---
+++ src/components/Tasks/TaskCard/TaskCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Task } from '../../../types/board';
+import './TaskCard.css';
+
+export interface TaskCardProps {
+  task: Task;
+  boardId: string;
+}
+export const TaskCard: React.FC<TaskCardProps> = ({ boardId, task }) => {
+  console.log(boardId);
+  return (
+    <div className='TaskCard'>
+      <div className='TaskCardHeader'>{task.name}</div>
+      <div className='TaskCardContent'>{task.description}</div>
+    </div>
+  );
+};
